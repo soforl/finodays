3906f8a543114526650d2d8aa3481f32d0c144a1
pro100bat9 <doroshenko.simon@mail.ru>
2023-08-29 02:42:57 +0300

    "feat: fixed everything"

---
+++ src/main/java/com/solution/proj/agreement/dto/request/RiskDto.java
@@ -0,0 +1,13 @@
+package com.solution.proj.agreement.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.processing.Generated;
+
+@Generated("jsonschema2pojo")
+@Getter
+@Setter
+public class RiskDto {
+    private String risk;
+}
